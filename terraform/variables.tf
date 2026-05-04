465ea142d530befd0890baa486df72fd4102552b
Briac <briac.leguillou@stellar.io>
2026-05-04 09:16:59 +0200

    fix: tf variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,34 @@
-variable "location"             { type = string; default = "westeurope" }
-variable "environment"          { type = string }
-variable "resource_group_name"  { type = string }
-variable "storage_account_name" { type = string }
-variable "keyvault_name"        { type = string }
-variable "tenant_id"            { type = string }
-variable "aks_node_count"       { type = number; default = 1 }
-variable "aks_vm_size"          { type = string; default = "Standard_B2s_v2" }
+variable "location" {
+  type    = string
+  default = "westeurope"
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "storage_account_name" {
+  type = string
+}
+
+variable "keyvault_name" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "aks_node_count" {
+  type    = number
+  default = 1
+}
+
+variable "aks_vm_size" {
+  type    = string
+  default = "Standard_B2s_v2"
+}
